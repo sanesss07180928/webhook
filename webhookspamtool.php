3f47990f82e920d498f748ee9c2bb0d691677744
桜町夏 <64882157+sanesss07180928@users.noreply.github.com>
2020-11-15 13:48:54 +0900

    Update webhookspamtool.php

--- webhookspamtool.php
+++ webhookspamtool.php
@@ -3,6 +3,6 @@ url:<input type="url" name="webhookurl"><br>
 回数:<input type="text" name="count"><br>
 name:<input type="username" name="name"><br>
 画像url(指定しなくていい):<input type="url" name="avatar"><br>
-メッセージ:<textarea name="msg">@everyone spam</textarea><br>
+メッセージ:<textarea name="msg">spam word</textarea><br>
 <input type="submit" value="送信">
 </form>
